c90df2d3a8d83fcc692b8d379014fdf80c7d92d2
Ariela Wenner <arielarei@proton.me>
2025-12-22 17:42:10 -0300

    release: 0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -87,7 +87,7 @@
 //!
 //! # license
 //!
-//! Copyright 2024 Ariela Wenner
+//! Copyright 2025 Ariela Wenner and myriam contributors
 //!
 //! Licensed under the Apache License, Version 2.0 (the "License");
 //! you may not use this file except in compliance with the License.
